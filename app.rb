35ed6bc2d91ad68eb33cc60d11f6af18368d1d97
wbosw <146214919+wbosw@users.noreply.github.com>
2023-10-16 19:13:21 +0000

    Dice game - full credit

--- app.rb
+++ app.rb
@@ -2,35 +2,60 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
-end
-
-get("/zebra") do
-  "We must add a route for each path we want to support"
+  erb(:home)
 end
 
 get("/rock") do
-  "
+  
   moves = ["rock", "paper", "scissors"]
 
-comp_move = moves.sample
+  @comp_move = moves.sample
 
-<h2>
-  We played rock!
-</h2>
+  if @comp_move == "rock"
+  @outcome = "tied"
+  elsif @comp_move == "paper"
+  @outcome = "lost"
+  elsif @comp_move == "scissors"
+  @outcome = "won"
+end
 
-<h2>
- They played #{comp_move}!
-</h2>
+erb(:rock)
 
-<h2>
-  We won!
-</h2>
-
-  "
 end
 
 
+get("/paper") do
+  
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+  @outcome = "won"
+  elsif @comp_move == "paper"
+  @outcome = "tied"
+  elsif @comp_move == "scissors"
+  @outcome = "lost"
+end
+
+erb(:paper)
+
+end
+
+get("/scissors") do
+  
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+  @outcome = "lost"
+  elsif @comp_move == "paper"
+  @outcome = "won"
+  elsif @comp_move == "scissors"
+  @outcome = "tied"
+end
+
+erb(:scissors)
+
+end
