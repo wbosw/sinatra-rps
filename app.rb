eba56fcc3d7512bce4a874d9c8c7e72f338f2958
wbosw <146214919+wbosw@users.noreply.github.com>
2023-10-16 17:54:47 +0000

    no color

--- app.rb
+++ app.rb
@@ -7,3 +7,30 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/zebra") do
+  "We must add a route for each path we want to support"
+end
+
+get("/rock") do
+  "
+  moves = ["rock", "paper", "scissors"]
+
+comp_move = moves.sample
+
+<h2>
+  We played rock!
+</h2>
+
+<h2>
+ They played #{comp_move}!
+</h2>
+
+<h2>
+  We won!
+</h2>
+
+  "
+end
+
+
